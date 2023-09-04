7820a32fb89614b9742f770fda52925fc60fac45
Adriana Carolina Vega Ahumada <carolinavegaahumada@gmail.com>
2023-09-04 17:51:07 +0000

    footer

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Footer = () => {
 return (
     <nav class="navbar navbar-light bg-dark">
-        <div class="container-fluid">
-        <span class="navbar-brand mb-0 h1">Navbar</span>
+        <div class="container-fluid d-flex justify-content-center">
+        <span class="text-white bg-dark">Copyright @ Your Website 2023 </span>
         </div>
     </nav>);
 }
